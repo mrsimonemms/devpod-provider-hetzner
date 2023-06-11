ae961d6db15d96478181304afe436c92d31b6207
Simon Emms <simon@simonemms.com>
2023-06-11 21:00:42 +0000

    feat(pkg): complete the options builder
    This is taken from the DigitalOcean builder as a first pass

--- pkg/options/options.go
+++ pkg/options/options.go
@@ -6,15 +6,51 @@ import (
 )
 
 type Options struct {
-	Token string
+	MachineID     string
+	MachineFolder string
+
+	Region      string
+	DiskImage   string
+	DiskSize    string
+	MachineType string
+	Token       string
 }
 
 func FromEnv(skipMachine bool) (*Options, error) {
 	retOptions := &Options{}
 
 	var err error
+	if !skipMachine {
+		retOptions.MachineID, err = fromEnvOrError("MACHINE_ID")
+		if err != nil {
+			return nil, err
+		}
+		// prefix with devpod-
+		retOptions.MachineID = "devpod-" + retOptions.MachineID
+
+		retOptions.MachineFolder, err = fromEnvOrError("MACHINE_FOLDER")
+		if err != nil {
+			return nil, err
+		}
+	}
 
 	retOptions.Token, err = fromEnvOrError("TOKEN")
+	if err != nil {
+		return nil, err
+	}
+	retOptions.DiskSize, err = fromEnvOrError("DISK_SIZE")
+	if err != nil {
+		return nil, err
+	}
+	retOptions.DiskImage, err = fromEnvOrError("DISK_IMAGE")
+	if err != nil {
+		return nil, err
+	}
+	retOptions.MachineType, err = fromEnvOrError("MACHINE_TYPE")
+	if err != nil {
+		return nil, err
+	}
+	retOptions.Region, err = fromEnvOrError("REGION")
 	if err != nil {
 		return nil, err
 	}
