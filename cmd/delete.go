357849b419198c92f3419a83af6650750b78f1b6
Simon Emms <simon@simonemms.com>
2023-06-16 21:15:35 +0000

    feat(pkg): add delete command

--- cmd/delete.go
+++ cmd/delete.go
@@ -16,6 +16,13 @@ limitations under the License.
 package cmd
 
 import (
+	"context"
+	"time"
+
+	"github.com/loft-sh/devpod/pkg/client"
+	"github.com/loft-sh/devpod/pkg/log"
+	"github.com/mrsimonemms/devpod-provider-hetzner/pkg/hetzner"
+	"github.com/mrsimonemms/devpod-provider-hetzner/pkg/options"
 	"github.com/spf13/cobra"
 )
 
@@ -23,6 +30,35 @@ import (
 var deleteCmd = &cobra.Command{
 	Use:   "delete",
 	Short: "Delete an instance",
+	RunE: func(_ *cobra.Command, args []string) error {
+		options, err := options.FromEnv(false)
+		if err != nil {
+			return err
+		}
+
+		ctx := context.Background()
+		hetznerClient := hetzner.NewHetzner(options.Token)
+
+		err = hetznerClient.Delete(ctx, options.MachineID)
+		if err != nil {
+			return err
+		}
+
+		// Wait until delete
+		for {
+			status, err := hetznerClient.Status(context.Background(), options.MachineID)
+			if err != nil {
+				log.Default.Errorf("Error retrieving server status: %v", err)
+				break
+			} else if status == client.StatusNotFound {
+				break
+			}
+
+			time.Sleep(time.Second)
+		}
+
+		return nil
+	},
 }
 
 func init() {
