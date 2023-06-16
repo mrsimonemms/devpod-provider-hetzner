38373ca49a17f2c3c3100c5a6044345d942153c6
Simon Emms <simon@simonemms.com>
2023-06-16 20:22:24 +0000

    feat(pkg): add status command

--- cmd/status.go
+++ cmd/status.go
@@ -16,6 +16,12 @@ limitations under the License.
 package cmd
 
 import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/mrsimonemms/devpod-provider-hetzner/pkg/hetzner"
+	"github.com/mrsimonemms/devpod-provider-hetzner/pkg/options"
 	"github.com/spf13/cobra"
 )
 
@@ -23,6 +29,20 @@ import (
 var statusCmd = &cobra.Command{
 	Use:   "status",
 	Short: "Retrieve the status of an instance",
+	RunE: func(_ *cobra.Command, args []string) error {
+		options, err := options.FromEnv(false)
+		if err != nil {
+			return err
+		}
+
+		status, err := hetzner.NewHetzner(options.Token).Status(context.Background(), options.MachineID)
+		if err != nil {
+			return err
+		}
+
+		_, err = fmt.Fprint(os.Stdout, status)
+		return err
+	},
 }
 
 func init() {
