e0f1c4b57f84780da3667aff837216c5ff080e41
Simon Emms <simon@simonemms.com>
2023-06-16 20:37:33 +0000

    feat(pkg): add stop command

--- cmd/stop.go
+++ cmd/stop.go
@@ -16,6 +16,13 @@ limitations under the License.
 package cmd
 
 import (
+	"context"
+	"time"
+
+	"github.com/loft-sh/devpod/pkg/client"
+	"github.com/loft-sh/devpod/pkg/log"
+	"github.com/mrsimonemms/devpod-provider-hetzner/pkg/hetzner"
+	"github.com/mrsimonemms/devpod-provider-hetzner/pkg/options"
 	"github.com/spf13/cobra"
 )
 
@@ -23,6 +30,36 @@ import (
 var stopCmd = &cobra.Command{
 	Use:   "stop",
 	Short: "Stop an instance",
+	RunE: func(_ *cobra.Command, args []string) error {
+		options, err := options.FromEnv(false)
+		if err != nil {
+			return err
+		}
+
+		ctx := context.Background()
+
+		hetznerClient := hetzner.NewHetzner(options.Token)
+
+		err = hetznerClient.Stop(ctx, options.MachineID)
+		if err != nil {
+			return err
+		}
+
+		// Wait until it's stopped
+		for {
+			status, err := hetznerClient.Status(ctx, options.MachineID)
+			if err != nil {
+				log.Default.Errorf("Error retrieving server status: %v", err)
+				break
+			} else if status == client.StatusStopped {
+				break
+			}
+
+			time.Sleep(time.Second)
+		}
+
+		return nil
+	},
 }
 
 func init() {
