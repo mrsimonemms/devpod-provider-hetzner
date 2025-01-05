8e72a147771d63d45296a4f92d78de158458d9ac
Simon Emms <simon@simonemms.com>
2025-01-05 16:39:17 +0000

    chore: update the copyright text

--- hack/provider/main.go
+++ hack/provider/main.go
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 Simon Emms <simon@simonemms.com>
+ * Copyright 2023 Simon Emms <simon@simonemms.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
