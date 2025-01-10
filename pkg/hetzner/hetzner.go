e3bf3954858876ffdf0fa0416f22192964d89a64
Simon Emms <simon@simonemms.com>
2025-01-10 14:21:36 +0000

    feat: improve the server creation process

--- pkg/hetzner/hetzner.go
+++ pkg/hetzner/hetzner.go
@@ -192,17 +192,37 @@ func (h *Hetzner) Create(ctx context.Context, req *hcloud.ServerCreateOpts, disk
 		return err
 	}
 
-	log.Default.Info("Server created - waiting until provisioned")
+	log.Default.Info("Server creation triggered")
+
+	actions := append([]*hcloud.Action{server.Action}, server.NextActions...)
+
+	for _, a := range actions {
+		log.Default.Debugf("Waiting for action to complete: %s", a.Command)
+		if err := h.waitForActionCompletion(ctx, a, time.Minute*5); err != nil {
+			log.Default.Errorf("Error in server creation action: %s, %s", a.Command, err)
+			return err
+		}
+	}
+
+	log.Default.Info("Server created - provisioning")
+
+	attempt := 0
 
 	for {
+		if attempt >= maxServerConnectAttempts {
+			return fmt.Errorf("exceeded attempts to connect to server: %d", attempt)
+		}
+		attempt++
+		log.Default.Debugf("Attempt %d of %d", attempt, maxServerConnectAttempts)
+
 		time.Sleep(time.Second)
 
-		log.Default.Info("Checking server provision status")
+		log.Default.Debug("Checking server provision status")
 
 		// Check the server is provisioned - this runs "ssh user@path cloud-init status"
 		sshClient, err := ssh.NewSSHClient(SSH_USERNAME, fmt.Sprintf("%s:%d", server.Server.PublicNet.IPv4.IP, SSH_PORT), privateKeyFile)
 		if err != nil {
-			log.Default.Errorf("Unable to connect to server: %v", err)
+			log.Default.Warnf("Unable to connect to server: %v", err)
 			continue
 		}
 		defer func() {
@@ -226,7 +246,7 @@ func (h *Hetzner) Create(ctx context.Context, req *hcloud.ServerCreateOpts, disk
 			break
 		}
 
-		log.Default.Info("Server not yet provisioned")
+		log.Default.Debug("Server not yet provisioned")
 	}
 
 	log.Default.Info("Server provisioned")
@@ -370,6 +390,57 @@ func (h *Hetzner) volumeByName(ctx context.Context, name string) (*hcloud.Volume
 	}
 
 	return volumes[0], nil
+}
+
+// waitForActionCompletion if a command returns an *hcloud.Action struct, this is a long-running job on the Hetzner side.
+// If we need to wait for it to finish to rely upon it later, this command will wait until we have success or that
+// there's an error
+func (h *Hetzner) waitForActionCompletion(ctx context.Context, action *hcloud.Action, timeout ...time.Duration) error {
+	if action == nil {
+		log.Default.Debug("No action received")
+		return nil
+	}
+
+	if len(timeout) == 0 {
+		timeout = []time.Duration{
+			time.Minute,
+		}
+	}
+
+	startTime := time.Now()
+	timeoutTime := startTime.Add(timeout[0])
+
+	for {
+		time.Sleep(time.Second)
+
+		now := time.Now()
+
+		if now.After(timeoutTime) {
+			log.Default.Errorf("action timedout: %d", action.ID)
+
+			return fmt.Errorf("action timed out")
+		}
+
+		log.Default.Debug("Checking action status")
+
+		status, _, err := h.client.Action.GetByID(ctx, action.ID)
+		if err != nil {
+			return err
+		}
+
+		log.Default.Debugf("Current status: %s", status.Status)
+
+		if status.Status == hcloud.ActionStatusError {
+			log.Default.Errorf("error completing action - code: %s, message: %s", status.ErrorCode, status.ErrorMessage)
+			return fmt.Errorf("%s: %s", status.ErrorCode, status.ErrorMessage)
+		}
+
+		if status.Status == hcloud.ActionStatusSuccess {
+			break
+		}
+	}
+
+	return nil
 }
 
 func generateSSHKeyFingerprint(publicKey string) (fingerprint string, err error) {
