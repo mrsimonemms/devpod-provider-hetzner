6a76de7c5710edbf2425b79fda01925479561b72
Simon Emms <simon@simonemms.com>
2025-01-10 16:11:22 +0000

    fix: set log level if provided

--- cmd/root.go
+++ cmd/root.go
@@ -20,6 +20,8 @@ import (
 	"os"
 
 	"github.com/loft-sh/log"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -29,6 +31,15 @@ var rootCmd = &cobra.Command{
 	Version: Version,
 	PersistentPreRunE: func(cobraCmd *cobra.Command, args []string) error {
 		log.Default.MakeRaw()
+
+		logLevel := os.Getenv("DEVPOD_LOG_LEVEL")
+		if logLevel != "" {
+			if lvl, err := logrus.ParseLevel(logLevel); err != nil {
+				log.Default.Error(errors.Wrap(err, "invalid log level provided, continuing"))
+			} else {
+				log.Default.SetLevel(lvl)
+			}
+		}
 
 		return nil
 	},
