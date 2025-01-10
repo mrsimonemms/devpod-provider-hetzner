f434652f82b729beda6a3b72b0036f7dae7581f8
Simon Emms <simon@simonemms.com>
2025-01-10 17:48:39 +0000

    refactor: get the locations at build time

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/cobra v1.8.1
 	gopkg.in/yaml.v3 v3.0.1
+	sigs.k8s.io/yaml v1.4.0
 )
 
 require (
--- hack/provider/main.go
+++ hack/provider/main.go
@@ -17,20 +17,26 @@
 package main
 
 import (
+	"context"
 	"crypto/sha256"
 	"encoding/hex"
 	"fmt"
 	"io"
 	"os"
 	"strings"
+
+	"github.com/hetznercloud/hcloud-go/v2/hcloud"
+	"github.com/loft-sh/devpod/pkg/provider"
+	"github.com/loft-sh/devpod/pkg/types"
+	"sigs.k8s.io/yaml"
 )
 
 var checksumMap = map[string]string{
-	"./dist/devpod-provider-hetzner-linux-amd64":       "##CHECKSUM_LINUX_AMD64##",
-	"./dist/devpod-provider-hetzner-linux-arm64":       "##CHECKSUM_LINUX_ARM64##",
-	"./dist/devpod-provider-hetzner-darwin-amd64":      "##CHECKSUM_DARWIN_AMD64##",
-	"./dist/devpod-provider-hetzner-darwin-arm64":      "##CHECKSUM_DARWIN_ARM64##",
-	"./dist/devpod-provider-hetzner-windows-amd64.exe": "##CHECKSUM_WINDOWS_AMD64##",
+	"./dist/devpod-provider-hetzner-linux-amd64":       "CHECKSUM_LINUX_AMD64",
+	"./dist/devpod-provider-hetzner-linux-arm64":       "CHECKSUM_LINUX_ARM64",
+	"./dist/devpod-provider-hetzner-darwin-amd64":      "CHECKSUM_DARWIN_AMD64",
+	"./dist/devpod-provider-hetzner-darwin-arm64":      "CHECKSUM_DARWIN_ARM64",
+	"./dist/devpod-provider-hetzner-windows-amd64.exe": "CHECKSUM_WINDOWS_AMD64",
 }
 
 func main() {
@@ -40,22 +46,194 @@ func main() {
 		return
 	}
 
-	content, err := os.ReadFile("./hack/provider/provider.yaml")
+	token := os.Getenv("HCLOUD_TOKEN")
+	if token == "" {
+		fmt.Fprintln(os.Stderr, "Expected HCLOUD_TOKEN environment variable")
+		os.Exit(1)
+	}
+
+	ctx := context.TODO()
+	h := hcloud.NewClient(hcloud.WithToken(token))
+
+	locations, err := h.Location.All(ctx)
 	if err != nil {
 		panic(err)
 	}
 
-	replaced := strings.Replace(string(content), "##VERSION##", os.Args[1], -1)
-	for k, v := range checksumMap {
-		checksum, err := File(k)
-		if err != nil {
-			panic(fmt.Errorf("generate checksum for %s: %v", k, err))
-		}
-
-		replaced = strings.Replace(replaced, v, checksum, -1)
+	regions := make([]string, 0)
+	for _, l := range locations {
+		regions = append(regions, l.Name)
 	}
 
-	fmt.Print(replaced)
+	serverTypes, err := h.ServerType.All(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	machineTypes := make([]string, 0)
+	for _, t := range serverTypes {
+		machineTypes = append(machineTypes, t.Name)
+	}
+
+	checksums := map[string]string{}
+	for filePath, v := range checksumMap {
+		checksum, err := File(filePath)
+		if err != nil {
+			panic(fmt.Errorf("generate checksum for %s: %v", filePath, err))
+		}
+
+		checksums[v] = checksum
+	}
+
+	version := fmt.Sprintf("v%s", os.Args[1])
+
+	s, err := yaml.Marshal(BuildConfig(version, checksums, "nbg1", regions, "cx32", machineTypes))
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Print(string(s))
+}
+
+func BuildConfig(version string, checksum map[string]string, defaultRegion string, regions []string, defaultMachineType string, machineTypes []string) provider.ProviderConfig {
+	releaseUrlBase := fmt.Sprintf("https://github.com/mrsimonemms/devpod-provider-hetzner/releases/download/%s", version)
+
+	return provider.ProviderConfig{
+		Name:        "hetzner",
+		Version:     version,
+		Description: "DevPod on Hetzner",
+		Icon:        "https://raw.githubusercontent.com/mrsimonemms/devpod-provider-hetzner/main/assets/hetzner.png",
+		OptionGroups: []provider.ProviderOptionGroup{
+			{
+				Name: "Hetzner options",
+				Options: []string{
+					"DISK_SIZE",
+					"DISK_IMAGE",
+					"MACHINE_TYPE",
+				},
+			},
+			{
+				Name: "Agent options",
+				Options: []string{
+					"AGENT_PATH",
+					"AGENT_DATA_PATH",
+					"INACTIVITY_TIMEOUT",
+					"INJECT_DOCKER_CREDENTIALS",
+					"INJECT_GIT_CREDENTIALS",
+				},
+			},
+		},
+		Options: map[string]*types.Option{
+			"REGION": {
+				Description: "The Hetzner region to use. E.g. nbg1",
+				Required:    true,
+				Default:     defaultRegion,
+				Suggestions: regions,
+			},
+			"DISK_SIZE": {
+				Description: "The disk size in GB.",
+				Default:     "30",
+			},
+			"DISK_IMAGE": {
+				Description: "The disk image to use.",
+				Default:     "docker-ce",
+			},
+			"MACHINE_TYPE": {
+				Description: "The machine type to use.",
+				Default:     defaultMachineType,
+				Suggestions: machineTypes,
+			},
+			"INACTIVITY_TIMEOUT": {
+				Description: "If defined, will automatically stop the VM after the inactivity period.",
+				Default:     "10m",
+			},
+			"INJECT_GIT_CREDENTIALS": {
+				Description: "If DevPod should inject git credentials into the remote host.",
+				Default:     "true",
+			},
+			"INJECT_DOCKER_CREDENTIALS": {
+				Description: "If DevPod should inject docker credentials into the remote host.",
+				Default:     "true",
+			},
+			"AGENT_PATH": {
+				Description: "The path where to inject the DevPod agent to.",
+				Default:     "/home/devpod/.devpod/devpod",
+			},
+			"AGENT_DATA_PATH": {
+				Description: "The path where to store the agent data.",
+				Default:     "/home/devpod/.devpod/agent",
+			},
+		},
+		Agent: provider.ProviderAgentConfig{
+			Path:                    "${AGENT_PATH}",
+			DataPath:                "${AGENT_DATA_PATH}",
+			Timeout:                 "${INACTIVITY_TIMEOUT}",
+			InjectGitCredentials:    "${INJECT_GIT_CREDENTIALS}",
+			InjectDockerCredentials: "${INJECT_DOCKER_CREDENTIALS}",
+			Binaries: map[string][]*provider.ProviderBinary{
+				"HETZNER_PROVIDER": {
+					{
+						OS:       "linux",
+						Arch:     "amd64",
+						Path:     fmt.Sprintf("%s/devpod-provider-hetzner-linux-amd64", releaseUrlBase),
+						Checksum: checksum["CHECKSUM_LINUX_AMD64"],
+					},
+					{
+						OS:       "linux",
+						Arch:     "arm64",
+						Path:     fmt.Sprintf("%s/devpod-provider-hetzner-linux-arm64", releaseUrlBase),
+						Checksum: checksum["CHECKSUM_LINUX_ARM64"],
+					},
+				},
+			},
+			Exec: provider.ProviderAgentConfigExec{
+				Shutdown: types.StrArray{"${HETZNER_PROVIDER} stop"},
+			},
+		},
+		Binaries: map[string][]*provider.ProviderBinary{
+			"HETZNER_PROVIDER": {
+				{
+					OS:       "linux",
+					Arch:     "amd64",
+					Path:     fmt.Sprintf("%s/devpod-provider-hetzner-linux-amd64", releaseUrlBase),
+					Checksum: checksum["CHECKSUM_LINUX_AMD64"],
+				},
+				{
+					OS:       "linux",
+					Arch:     "arm64",
+					Path:     fmt.Sprintf("%s/devpod-provider-hetzner-linux-arm64", releaseUrlBase),
+					Checksum: checksum["CHECKSUM_LINUX_ARM64"],
+				},
+				{
+					OS:       "darwin",
+					Arch:     "amd64",
+					Path:     fmt.Sprintf("%s/devpod-provider-hetzner-darwin-amd64", releaseUrlBase),
+					Checksum: checksum["CHECKSUM_DARWIN_AMD64"],
+				},
+				{
+					OS:       "darwin",
+					Arch:     "arm64",
+					Path:     fmt.Sprintf("%s/devpod-provider-hetzner-darwin-arm64", releaseUrlBase),
+					Checksum: checksum["CHECKSUM_DARWIN_ARM64"],
+				},
+				{
+					OS:       "windows",
+					Arch:     "arm64",
+					Path:     fmt.Sprintf("%s/devpod-provider-hetzner-windows-amd64.exe", releaseUrlBase),
+					Checksum: checksum["CHECKSUM_WINDOWS_AMD64"],
+				},
+			},
+		},
+		Exec: provider.ProviderCommands{
+			Init:    types.StrArray{"${HETZNER_PROVIDER} init"},
+			Command: types.StrArray{"${HETZNER_PROVIDER} command"},
+			Create:  types.StrArray{"${HETZNER_PROVIDER} create"},
+			Delete:  types.StrArray{"${HETZNER_PROVIDER} delete"},
+			Start:   types.StrArray{"${HETZNER_PROVIDER} start"},
+			Stop:    types.StrArray{"${HETZNER_PROVIDER} stop"},
+			Status:  types.StrArray{"${HETZNER_PROVIDER} status"},
+		},
+	}
 }
 
 // File hashes a given file to a sha256 string
